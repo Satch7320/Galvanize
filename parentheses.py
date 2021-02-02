d7bad3f248125aaf912c79bf274f4f11a04697ae
satch7320 <joseph.aylsworth@gmail.com>
2021-02-02 12:30:00 -0600

    whoops, should be proper changes

--- parentheses.py
+++ parentheses.py
@@ -1,41 +1,27 @@
 import string
-def checkparens(in_str):
-    parens = 0
-    brackets = 0
-    curlies = 0
-    most_recent = []
+def check_parens(in_str):
+    opens = '([{\'\"'
+    closes = ')]}\'\"'
+    stack = []
     for char in in_str:
-        if char == '(':
-            parens += 1
-            most_recent.append('(')
-        if char == ')':
-            parens -= 1
-            if parens < 0 or most_recent.pop() != '(':
-                return False
-        if char == '[':
-            brackets += 1
-            most_recent.append('[')
-        if char == ']':
-            brackets -= 1
-            if brackets < 0 or most_recent.pop() != '[':
-                return False
-        if char == '{':
-            curlies += 1
-            most_recent.append('{')
-        if char == '}':
-            curlies -= 1
-            if curlies < 0 or most_recent.pop() != '{':
-                return False
-    if parens == 0 and brackets == 0 and curlies == 0:
-        return True
-    return False
+        if char in opens:
+            stack.append(opens.index(char))
+        if char in closes:
+            if stack:
+                if stack[-1] == closes.index(char):
+                    stack.pop() 
+                else:
+                    return False
+            else: 
+                return False   
+    return stack == []
 
-print(checkparens('(a(b))()()'))
-print(checkparens('a(b())'))
-print(checkparens(')a(b))'))
-print(checkparens('(a(b){c}'))
-print(checkparens('(a(b)c})'))
-print(checkparens('[(a(b){c})]'))
-print(checkparens('(a(b)]{c}'))
-print(checkparens(')b('))
-print(checkparens('(a[b)c]'))
+print(check_parens('(a(b))()()'))
+print(check_parens('a(b())'))
+print(check_parens(')a(b))'))
+print(check_parens('(a(b){c}'))
+print(check_parens('(a(b)c})'))
+print(check_parens('[(a(b){c})]'))
+print(check_parens('(a(b)]{c}'))
+print(check_parens(')b('))
+print(check_parens('(a[b)c]'))
