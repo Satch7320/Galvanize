869cbebdb3047a74b3824711e25a9f1f44b206f6
satch7320 <joseph.aylsworth@gmail.com>
2021-02-23 11:39:11 -0600

    restructure directory

---
+++ capstone1/Capstone_1.md
@@ -0,0 +1,4 @@
+# Capstone 1: Kickstarter Projects
+## Exploratory Data Analysis
+### Header 3
+#### Header 4
---
+++ capstone1/cp1.py
@@ -0,0 +1,31 @@
+import numpy as np
+import pandas as pd
+import scipy.stats as stats
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+late_csv = pd.read_csv('ks-projects-201801.csv')
+
+def make_hist(x):
+    fig, ax = plt.subplots()
+    return ax.hist(x)
+
+def subtable(category):
+    df = late_csv[late_csv['main_category'] == category]
+    return df
+
+cats = set(late_csv['main_category'])
+sub_cats = set(late_csv['category'])
+state_number_map = {'failed':0, 'successful':1, 'live':2, 
+                    'suspended':3, 'canceled':4, 'undefined':5}
+                    
+late_csv['launched'] = pd.to_datetime(late_csv['launched'])
+late_csv['deadline'] = pd.to_datetime(late_csv['deadline'])
+late_csv['state'] = late_csv['state'].map(state_number_map)
+late_csv['length'] = late_csv['deadline'] - late_csv['launched']
+
+av_pledge = late_csv.groupby('main_category')
+backers_counts = av_pledge['backers'].count()
+
+f_and_v = subtable('Film & Video')
